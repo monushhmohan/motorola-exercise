7bfdf1ddc738f9bf7f190d710e4966f585e2ddeb
Your Name <you@example.com>
2021-08-26 08:02:24 +0100

    create new appium driver and desired capability instance

--- motorola-appium/src/test/java/tests/motorolaExercise.java
+++ motorola-appium/src/test/java/tests/motorolaExercise.java
@@ -1,5 +1,13 @@
 package tests;
 
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
 public class motorolaExercise {
+	
+	AppiumDriver<MobileElement> driver;
+	DesiredCapabilities caps = new DesiredCapabilities();
 
 }
